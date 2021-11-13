9af01cdc1e71523a2e5049b50b0bab8a41b3450e
karishma kunwar <57294417+kari-hue@users.noreply.github.com>
2021-11-13 19:10:20 +0545

    Add files via upload
    Updated code added

--- NLP_Streamlit_application/NLP_on_a_go.py
+++ NLP_Streamlit_application/NLP_on_a_go.py
@@ -5,6 +5,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import re
+from rouge import Rouge
+from rouge import FilesRouge
+from nltk.translate.bleu_score import sentence_bleu
+import argparse
+
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
@@ -115,8 +120,23 @@ def sumy_summarizer(docx):
     return result
 
 
+# Function that will implement rogue metrics
 
-## NER
+def rogueEvaluation(hypothesis, reference):
+    files_rouge = FilesRouge()
+    scores = files_rouge.get_scores(hypothesis, reference)
+    return(scores)
+
+
+## Function that will implement Bleu Metrics
+def argparser():
+    Argparser = argparse.ArgumentParser()
+    Argparser.add_argument('--reference', type=str, default='newsummary.txt', help='Reference File')
+    Argparser.add_argument('--candidate', type=str, default='ref.txt', help='Candidate file')
+
+    args = Argparser.parse_args()
+    return args
+
 
 
 # @st.cache(allow_output_mutation=True)
@@ -141,11 +161,12 @@ def main():
     # Text summarization
     if choice == "Summarizer":
         st.subheader("Summarize your text")
-        message = st.text_area("your text here", "Type Here")
+        message = st.text_area("your text here", "Type here")
         summary_options = st.selectbox(
             "Choose your summarizer", ("nltk", "gensim", "sumy"))
-        
-        text_range= st.sidebar.slider("Summarize words Range, For Gensim only",25,500)
+
+        text_range = st.sidebar.slider(
+            "Summarize words Range, For Gensim only", 25, 500)
 
         if st.button("summarize"):
             if summary_options == 'nltk':
@@ -159,28 +180,66 @@ def main():
             elif summary_options == 'sumy':
                 st.text("Using Sumy....")
                 summary_result = sumy_summarizer(message)
-            
-           
-
 
             else:
                 st.warning("Using Default summarizer")
                 st.text("Using Gensim")
-                summary_result = summarize(message,word_count=text_range)
-
+                summary_result = summarize(message, word_count=text_range)
+            
             st.success(summary_result)
+           
+            generated_summary = summary_result
+            file = open("newsummary.txt","w") 
+            file.write(generated_summary) 
+           
 
+            #reference_text = st.text_area("Type here","Type here")
+
+            #if st.button("Evaluate"):
+                #reference_summary = reference_text
+                
+                #file1 = open("reference.txt","a")
+                #file1.write(reference_summary)
+
+
+        ##Entering the human generated summary
+        st.subheader("Human generated summary")
+        ref_text = st.text_area("Enter your Text", "Type Here")
+        file1 = open("ref.txt","w")  ## Store the text in the textbox into ref.txt file
+        file1.write(ref_text) 
+        file1.close()
         
+        if st.button("Evaluate score with the human summary(Using Rogue Metrics)"):
+
+            hyp_path = "newsummary.txt"
+            ref_path = "ref.txt"
+           
+                
+            scores = rogueEvaluation(hyp_path, ref_path)   ## Calling the rogueEvaluation function
+            st.write(scores)
+        
+        if st.button("Evaluate score with the human summary(Using BLeu)"):
+            args = argparser()
+            reference = open(args.reference, 'r').readlines()
+            candidate = open(args.candidate, 'r').readlines()
+            if len(reference) != len(candidate):
+                raise ValueError('The number of sentences in both files do not match.')
+            score = 0.
+            for i in range(len(reference)):
+                    score += sentence_bleu([reference[i].strip().split()], candidate[i].strip().split())
+                    score /= len(reference)
+                    st.write("The bleu score is: "+str(score))
+
+
+           
 
     if choice == "NER":
         st.subheader('Named Entity Recognizer')
         raw_docx = st.text_area('Your Text')
         docx = nlp(raw_docx)
         if st.button('Analyze'):
-            spacy_streamlit.visualize_ner(docx, labels=nlp.get_pipe("ner").labels)
-        
-        
-            
+            spacy_streamlit.visualize_ner(
+                docx, labels=nlp.get_pipe("ner").labels)
 
     if choice == "Relationship":
         st.subheader("Extract the relationship between the text")
